258914c7d9d26371d38d4cb1ecbc1766f48145ed
jimbucc <James.Bucciferro@PERKINELMER.COM>
2025-11-07 06:44:23 -0500

    fix apostrophe error

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
   return (
     <section>
       <h1 className="text-center">
-        The Hub for Every Dev <br /> Event You Can't Miss
+        The Hub for Every Dev <br /> Event You Can&apos;t Miss
       </h1>
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in One Place
